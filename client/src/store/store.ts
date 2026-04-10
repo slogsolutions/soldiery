999da60631bfeb82db9553dec53c8b899748a28f
Himanshu Panwar <hpanwar875@gmail.com>
2026-04-10 22:41:11 +0530

    slices add : auth & task & assignment & soldier

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-const store = configureStore({
+import authReducer from "./slices/authSlice";
+import taskReducer from "./slices/taskSlice";
+import assignmentReducer from "./slices/taskSlice";
+import soldierReducer from "./slices/soldierSlice";
+
+export const store = configureStore({
   reducer: {
+    auth: authReducer,
+    tasks: taskReducer,
+    assignments: assignmentReducer,
+    soldiers: soldierReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type Dispatch = typeof store.dispatch;
-
-export default store;
+export type AppDispatch = typeof store.dispatch;
