7408d23a4f7314073ec7b80a8f9d29ae3d878a1b
Himanshu Panwar <hpanwar875@gmail.com>
2026-04-10 22:25:05 +0530

    add register & login route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+const Router = createBrowserRouter([
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+]);
 
 const App = () => {
-  return (
-    <div className="text-red-700">App</div>
-  )
-}
+  return <RouterProvider router={Router} />;
+};
 
-export default App
+export default App;
