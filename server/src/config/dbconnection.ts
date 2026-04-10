a1d505cc3b2ba03a8f943105da0f22393f0e421c
Himanshu Panwar <hpanwar875@gmail.com>
2026-04-10 10:02:58 +0530

    add dbconnection function

---
+++ server/src/config/dbconnection.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
+
+const dbConnection = async (): Promise<void> => {
+
+  try {
+    await mongoose.connect(
+      process.env.DATABASE_URL || ""    );
+      
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Database connection error:", error);
+    throw error;
+  }
+};
+
+export default dbConnection;
