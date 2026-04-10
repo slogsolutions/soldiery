1aae383b4036e35de1fe9bca16087c7b06739b9a
Himanshu Panwar <hpanwar875@gmail.com>
2026-04-10 23:14:26 +0530

    add task model

---
+++ server/src/models/Task.ts
@@ -0,0 +1,29 @@
+import mongoose ,{ Schema, model, Document, Types } from "mongoose";
+
+export interface ITask extends Document {
+  title: string;
+  description?: string;
+  createdBy: Types.ObjectId;
+  isActive?: Boolean;
+  createdAt:Date;
+  updatedAt:Date;
+}
+
+const taskSchema = new Schema<ITask>(
+  {
+    title: { type: String, required: true },
+    description: String,
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Task = mongoose.models.Task || model<ITask>("Task", taskSchema);
