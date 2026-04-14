899706279344383e27ae3543dfa7f15a373ff324
Himanshu Panwar <hpanwar875@gmail.com>
2026-04-14 22:45:51 +0530

    remove unused hook

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -2,21 +2,4 @@ import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "../store/store";
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector = <T>(selector: (state: RootState) => T) =>
-  useSelector(selector);
-
-// useAuth hook
-export const useAuth = () => {
-  const { user, token, loading, error } = useAppSelector((s) => s.auth);
-  return { user, token, loading, error, isAuthenticated: !!token };
-};
-
-// useRole hook
-export const useRole = () => {
-  const { user } = useAppSelector((s) => s.auth);
-  return {
-    isManager: user?.role === "manager",
-    isSoldier: user?.role === "soldier",
-    role: user?.role,
-  };
-};
+export const useAppSelector = <T>(selector: (state: RootState) => T) => useSelector(selector);
